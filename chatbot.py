59894bdd1b00f0dcd2004c4af43f4c68798e4a7b
sarah <158591005+Sarah1234n@users.noreply.github.com>
2025-04-29 10:25:54 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -154,7 +154,7 @@ with st.sidebar:
         st.button("Delete Chat", on_click=delete_chat)
 
 # Main Content
-st.title("Chatbot Application By Sarah N")
+st.title("Chatbot Application By Sarah")
 
 if st.session_state["current_chat"]:
     chat_id = st.session_state["current_chat"]
